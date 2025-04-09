44e8ad02f2e448ba43884ae213b3eaf2fe478f10
Sanghun <po4865@gmail.com>
2025-04-09 17:58:59 +0200

    test: (WIP) Add unit test
    - Added unit tests for each operators.
    
    ### Problem
    
    - Current test code cannot test for handling exception.

--- src/test/kotlin/SampleTest.kt
+++ src/test/kotlin/SampleTest.kt
@@ -1,14 +1,54 @@
-import org.example.Sample
+import org.example.*
 import org.junit.jupiter.api.Assertions.*
 import kotlin.test.Test
 
- class SampleTest {
+class CalculatorTest {
 
-     private val testSample: Sample = Sample()
+    private val testCalculator: Calculator = Calculator(
+        operators = listOf(Addition(), Subtraction(), Multiplication(), Division())
+    )
 
-     @Test
-     fun testSum() {
-         val expected = 42
-         assertEquals(expected, testSample.sum(40, 2))
-     }
- }
+    @Test
+    fun testAddition() {
+        val expected = 42
+        assertEquals(expected, testCalculator.calculate(40, '+', 2))
+    }
+
+    @Test
+    fun testSubtraction() {
+        val expected = 38
+        assertEquals(expected, testCalculator.calculate(40, '-', 2))
+    }
+
+    @Test
+    fun testMultiplication() {
+        val expected = 80
+        assertEquals(expected, testCalculator.calculate(40, '*', 2))
+    }
+
+    @Test
+    fun testDivision_01() {
+        val expected = 20
+        assertEquals(expected, testCalculator.calculate(40, '/', 2))
+    }
+
+    @Test
+    fun testDivision_02() {
+        val expected = 0
+        assertEquals(expected, testCalculator.calculate(0, '/', 2))
+    }
+
+    @Test
+    fun testDivisionByZero() {
+        val expected = ArithmeticException("/ by zero")
+        assertEquals(expected, testCalculator.calculate(40, '/', 0))
+    }
+
+    @Test
+    fun testUnsupportedOp() {
+        val op = '.'
+        val expected = IllegalArgumentException("Unsupported operator: $op")
+        assertEquals(expected, testCalculator.calculate(40, op, 0))
+    }
+
+}
