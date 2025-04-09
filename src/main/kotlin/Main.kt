b528ae3db44cfee848c0c2226e7ffdceea4335be
Sanghun <po4865@gmail.com>
2025-04-09 15:23:59 +0200

    refactor: Refine calculate() function to handle exceptions
    - Problem: Lack of error handling
       - Division by zero
       - Unsupported operator
    - Solution: Update function to throw 'IllegalArgumentException'

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,20 +5,22 @@ fun main() {
     println(calculate(40, '-', 2))
     println(calculate(40, '*', 2))
     println(calculate(40, '/', 2))
-    println(calculate(40, '.', 2))
     println(calculate(0, '/', 40))
-    println(calculate(40, '/', 0))
+//    println(calculate(40, '/', 0))
+//    println(calculate(40, '.', 2))
 }
 
 fun calculate(a: Int, op: Char, b: Int): Int {
-    val result = when (op) {
+    return when (op) {
         '+' -> a + b
         '-' -> a - b
         '*' -> a * b
-        '/' -> a / b
-        else -> 0
+        '/' -> {
+            if (b == 0) throw IllegalArgumentException("Division by zero.")
+            a / b
+        }
+        else -> throw IllegalArgumentException("Unsupported operator: $op")
     }
-    return result
 }
 
 class Sample() {
